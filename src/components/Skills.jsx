9adb0d8aec1069a1a727d55d2cfd4eae20fcc329
nomanchaudhary <nomanchaudhary1618@gmail.com>
2025-04-04 00:10:53 +0500

    Skill Section Update

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,13 +1,26 @@
-import { motion } from "framer-motion";
 import React from "react";
+import Html from '../assets/Html.png'
+import Css from '../assets/Css.png'
+import Javascript from '../assets/Javascript.png'
+import Reacticon from '../assets/React.png'
+import Redux from '../assets/Redux.png'
+import Tailwind from '../assets/Tailwind.png'
+import Framer from '../assets/Framer.png'
+import Github from '../assets/Github.png'
+import {motion} from 'framer-motion'
+
 
 function Skills() {
   const Skills = [
-    { title: "HTML", level: "85%" },
-    { title: "CSS", level: "75%" },
-    { title: "JavaScript", level: "75%" },
-    { title: "React JS", level: "70%" },
-    { title: "Tailwind CSS", level: "80%" },
+    { img: Html, title: "HTML" },
+    { img: Css, title: "CSS" },
+    { img: Javascript, title: "JavaScript" },
+    { img: Reacticon, title: "React JS" },
+    { img: Redux, title: "Tailwind CSS" },
+    { img: Tailwind, title: "Redux Toolkit" },
+    { img: Framer, title: "Framer Motion" },
+    { img: Github, title: "GitHub" },
+
   ];
   return (
     <>
@@ -17,26 +30,18 @@ function Skills() {
       <h2 className="text-3xl font-bold font-montserrat mt-10">
         Frontend Technologies && Tools
       </h2>
-      <div className=" w-full flex flex-wrap  gap-y-5 px-32 mt-10">
-        {Skills.map((skill, index) => (
-          <div className="w-full flex justify-center items-center  ">
-            <div
+      <div className="grid grid-cols-4 gap-6 m-10">
+        {Skills.map(({ img, title}, index) => (
+          <motion.div
             key={index}
-            className="w-full"
+            className="flex justify-center items-center p-1 gap-2 border-2 border-black rounded-2xl"
+            whileHover={{backgroundColor:"orange"}}
           >
-            <span className="font-bold text-black">{skill.title}</span>
-          </div>
-          <div className="w-full">
-          <motion.div className="bg-orange-400 h-6 flex justify-center items-center rounded-full"
-                initial={{ width: "0%" }}
-                animate={{ width: skill.level }}
-                transition={{ duration: 1, ease: "easeInOut" }}>
-                  <span className="font-bold font-montserrat text-center text-black">{skill.level}</span>
-                
-                </motion.div>
-          </div>
-          </div>
-          
+            <div className="text-xl font-montserrat font-semibold">
+              {title}
+            </div>
+            <img src={img} alt="Image is Loading" className="w-15 h-15" />
+          </motion.div>
         ))}
       </div>
     </>
