6d2253dfdd292d3a2abe3f9c3f760115eba91579
nomanchaudhary <nomanchaudhary1618@gmail.com>
2025-04-21 16:51:03 +0500

    Changes in experience section

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -59,20 +59,20 @@ function Experience() {
         ></motion.div>
         <ul className="w-full flex flex-col justify-between items-start ml-5 dark:bg-black dark:text-white">
           <Details
-            position="Junior Frontend Developer"
-            company="CodeLab IT Solutions"
-            companyLink="https://codelab.pk/"
-            location="Bahawalpur, Pakistan"
-            time="Jan 2021 – Aug 2021"
-            work="I developed responsive user interface components using HTML, CSS, and JavaScript, ensuring a seamless experience across various devices. I collaborated closely with backend developers to integrate APIs, which significantly enhanced the overall functionality of the website. Additionally, I focused on performance optimization, successfully reducing load times by 20%, leading to a faster and more efficient user experience."
-          />
-          <Details
             position="ReactJs Trainee"
             company="Techons Solutions"
             companyLink="https://www.linkedin.com/company/techons/"
             location="Lahore, Pakistan"
             time="Nov 2024 – Feb 2025"
             work="As a Trainee ReactJS Developer at Techons solutions I assisted in developing and optimizing React.js components for web applications. I implemented state management using Redux Toolkit and the Context API to ensure efficient data handling across components. Additionally, I contributed to enhancing the UI/UX by creating responsive and interactive web pages using Tailwind CSS, resulting in a more engaging user experience."
+          />
+          <Details
+            position="Junior Frontend Developer"
+            company="CodeLab IT Solutions"
+            companyLink="https://codelab.pk/"
+            location="Bahawalpur, Pakistan"
+            time="Jan 2021 – Aug 2021"
+            work="I developed responsive user interface components using HTML, CSS, and JavaScript, ensuring a seamless experience across various devices. I collaborated closely with backend developers to integrate APIs, which significantly enhanced the overall functionality of the website. Additionally, I focused on performance optimization, successfully reducing load times by 20%, leading to a faster and more efficient user experience."
           />
         </ul>
       </div>
